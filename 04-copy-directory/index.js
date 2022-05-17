055d5e3981140e4ee186376c24927287e69ad6e1
googray <shat.rom07@gmail.com>
2022-05-17 22:15:01 +0200

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const { stderr } = require('process');
 
+const srcDr = path.join(__dirname, 'files');
+const destDr = path.join(__dirname, 'files-copy');
+
+async function copyDir(srcDr, destDr) {
+  try {
+    await fs.mkdir(destDr, { recursive: true });
+    let entries = await fs.readdir(srcDr, {
+      withFileTypes: true,
+    });
+
+    for (let file of entries) {
+      let srcPath = path.join(srcDr, file.name);
+      let destPath = path.join(destDr, file.name);
+
+      file.isDirectory()
+        ? await copyDir(srcPath, destPath)
+        : await fs.copyFile(srcPath, destPath);
+    }
+  } catch (err) {
+    stderr.write(`${err}`);
+  }
+}
+
+copyDir(srcDr, destDr);
