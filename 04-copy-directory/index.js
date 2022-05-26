c395c64655ef2381f5208b255d229b29ce6d521b
googray <shat.rom07@gmail.com>
2022-05-26 12:19:49 +0200

    fix: error when files are deleted, non updates in dest folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,21 +7,26 @@ const destDr = path.join(__dirname, 'files-copy');
 
 async function copyDir(srcDr, destDr) {
   try {
-    await fs.mkdir(destDr, { recursive: true });
-    let entries = await fs.readdir(srcDr, {
-      withFileTypes: true,
-    });
+    await fs.rm(destDr, { recursive: true, force: true });
+    try {
+      await fs.mkdir(destDr, { recursive: true });
+      let entries = await fs.readdir(srcDr, {
+        withFileTypes: true,
+      });
 
-    for (let file of entries) {
-      let srcPath = path.join(srcDr, file.name);
-      let destPath = path.join(destDr, file.name);
+      for (let file of entries) {
+        let srcPath = path.join(srcDr, file.name);
+        let destPath = path.join(destDr, file.name);
 
-      file.isDirectory()
-        ? await copyDir(srcPath, destPath)
-        : await fs.copyFile(srcPath, destPath);
+        file.isDirectory()
+          ? await copyDir(srcPath, destPath)
+          : await fs.copyFile(srcPath, destPath);
+      }
+    } catch (err) {
+      stderr.write(`${err}`);
     }
   } catch (err) {
-    stderr.write(`${err}`);
+    stderr.write(`Error remove DR: ${err}`);
   }
 }
 
